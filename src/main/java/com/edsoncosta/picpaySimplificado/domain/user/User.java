8ff5e3d2fd4eb55fb6bcf577dfcce6fc07291d52
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2023-12-30 01:25:33 +0000

    feat:create User entity

---
+++ src/main/java/com/edsoncosta/picpaySimplificado/domain/user/User.java
@@ -0,0 +1,35 @@
+package com.edsoncosta.picpaySimplificado.domain.user;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+}
