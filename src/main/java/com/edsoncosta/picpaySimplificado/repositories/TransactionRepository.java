3a4b5190038e3302fe3e8094a0547e63bad1402a
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2023-12-30 01:27:12 +0000

    feat:update transaction repository

--- src/main/java/com/edsoncosta/picpaySimplificado/repositories/TransactionRepository.java
+++ src/main/java/com/edsoncosta/picpaySimplificado/repositories/TransactionRepository.java
@@ -1,4 +1,7 @@
 package com.edsoncosta.picpaySimplificado.repositories;
 
-public interface TransactionRepository {
+import com.edsoncosta.picpaySimplificado.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 }
