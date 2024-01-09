aa7cefb80ae9987d62d439606979f130eefab4a5
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2024-01-09 10:38:52 +0000

    feat:update Transaction properties

--- src/main/java/com/edsoncosta/picpaySimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/edsoncosta/picpaySimplificado/domain/transaction/Transaction.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private BigDecimal amount;
 
